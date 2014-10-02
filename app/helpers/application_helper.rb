80b7683a8f0ca67e2edc1150029a8308d403f08f
ppj <prasanna_3003@yahoo.com>
2014-10-03 01:06:18 +1000

    fix time display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def fix_url(url)
     url.starts_with?('http://') ? url : 'http://' + url
   end
+
+  def fix_time(time)
+    time.localtime.strftime("(%d-%b-%Y %I:%M%p %z)")
+  end
 end
