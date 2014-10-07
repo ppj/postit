41271c2a895e0df5e1a580688e1773940b1c95f7
ppj <prasanna_3003@yahoo.com>
2014-10-07 15:40:18 +1100

    added authentication to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_secure_password validations: false
+
   validates :username, length: {minimum: 3}, uniqueness: true
+  validates :password, length: {minimum: 3}
 end
