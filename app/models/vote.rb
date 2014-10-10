e312b7c8ad11dd10e90db1391e31cb4a4e26cec2
ppj <prasanna_3003@yahoo.com>
2014-10-10 12:37:07 +1100

    simplified validation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   belongs_to :voteable, polymorphic: true
 
-  validates :user_id, :uniqueness => {
-        :scope => [:voteable_id, :voteable_type]
-    }
+  validates_uniqueness_of :creator, scope: :voteable
 end
