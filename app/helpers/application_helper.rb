d85b98067ba43e02afe99f6c21928e8a1032f635
ppj <prasanna_3003@yahoo.com>
2014-10-11 19:13:45 +1100

    added helpers for creating vote-up vote-down or vote-delete links for comments and posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,31 @@ module ApplicationHelper
   def fix_time(time)
     time.localtime.strftime("(%d-%b-%Y %I:%M%p %z)")
   end
+
+  def link_based_on_current_users_vote_on_post(post_object, value)
+    if Vote.find_by(creator: current_user, vote: value, voteable: post_object)
+      link_to vote_destroy_post_path(post_object), method: 'delete' do
+        "<i class='icon-fire'></i>".html_safe
+      end
+    else
+      icon = value ? "<i class='icon-thumbs-up'></i>" : "<i class='icon-thumbs-down'></i>"
+      link_to vote_post_path(post_object, vote: value), method: 'post' do
+        icon.html_safe
+      end
+    end
+  end
+
+  def link_based_on_current_users_vote_on_comment(comment_object, value)
+    if Vote.find_by(creator: current_user, vote: value, voteable: comment_object)
+      link_to vote_destroy_post_comment_path(comment_object.post, comment_object), method: 'delete' do
+        "<i class='icon-fire'></i>".html_safe
+      end
+    else
+      icon = value ? "<i class='icon-thumbs-up'></i>" : "<i class='icon-thumbs-down'></i>"
+      link_to vote_post_comment_path(comment_object.post, comment_object, vote: value), method: 'post' do
+        icon.html_safe
+      end
+    end
+  end
+
 end
