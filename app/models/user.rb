c73fcff0f857e6bca4d435d8c60fa353a9613cfa
ppj <prasanna_3003@yahoo.com>
2014-10-16 14:45:29 +1100

    slugified user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,23 @@ class User < ActiveRecord::Base
 
   has_secure_password validations: false
 
+  before_save :generate_slug
+
   validates :username, length: {minimum: 3}, uniqueness: true
   validates :password, on: :create, length: {minimum: 3}
 
   def admin?
     self.role == 'admin'
   end
+
+  def to_param
+    self.slug
+  end
+
+  private
+
+  def generate_slug
+    self.slug = self.username.gsub(/\s+/,'_').gsub(/\W/,'').gsub(/_+/,'_').downcase
+  end
+
 end
