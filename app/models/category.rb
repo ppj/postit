d5999a32061c0d04b9722040f5bbb929bd592725
ppj <prasanna_3003@yahoo.com>
2014-10-16 14:54:30 +1100

    slugified category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,18 @@ class Category < ActiveRecord::Base
   has_many :post_categories
   has_many :posts, through: :post_categories
 
+  before_save :generate_slug
+
   validates :name, length: {minimum: 4}, uniqueness: true
+
+  def to_param
+    self.slug
+  end
+
+  private
+
+  def generate_slug
+    self.slug = self.name.gsub(/\s+/,'_').gsub(/\W/,'').gsub('_','-').gsub(/-+$/,'').gsub(/^-+/,'').gsub(/-+/,'-').downcase
+  end
+
 end
